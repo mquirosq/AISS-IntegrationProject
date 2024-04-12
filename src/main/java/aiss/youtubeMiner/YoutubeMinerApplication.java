cf255a43797a51d817277409da65e827a35476a8
maquirosq <mquirosquiroga@gmail.com>
2024-04-12 15:12:31 +0200

    Added VimeoMiner and YoutubeMiner

---
+++ src/main/java/aiss/youtubeMiner/YoutubeMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss.youtubeMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeMinerApplication.class, args);
+	}
+
+}
