171a5fc547ba13349468d0a0045e1d4210bbc7a3
maquirosq <mquirosquiroga@gmail.com>
2024-04-27 17:29:27 +0200

    Implemented POST consumption from VideoMiner #14

--- src/main/java/aiss/vimeoMiner/service/ChannelService.java
+++ src/main/java/aiss/vimeoMiner/service/ChannelService.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -15,6 +17,7 @@ public class ChannelService {
     @Autowired
     RestTemplate restTemplate;
 
+    // Get from Vimeo API
     public Channel getChannel(String channelId){
         // URI
         String uri = "https://api.vimeo.com/channels/" + channelId;
@@ -32,5 +35,19 @@ public class ChannelService {
         return response.getBody();
     }
 
+    // Post to VideoMiner:
+    public Channel createChannel(Channel channel){
+        String uri = "localhost:8080/api/v1/channels";
+        try {
+            HttpEntity<Channel> request = new HttpEntity<>(channel);
+            ResponseEntity<Channel> response = restTemplate.exchange(uri, HttpMethod.POST, request, Channel.class);
+            Channel createdChannel = response.getBody();
+            return createdChannel;
+        }
+        catch(RestClientResponseException err) {
+            System.out.println("Error when creating the channel " + channel + ":"+ err.getLocalizedMessage());
+            return null;
+        }
+    }
 
 }
