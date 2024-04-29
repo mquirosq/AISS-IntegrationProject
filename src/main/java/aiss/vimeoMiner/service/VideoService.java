0c7b9e718f50fbe88233c7fa15eb04e80ab832ee
maquirosq <mquirosquiroga@gmail.com>
2024-04-29 19:20:50 +0200

    Added videos service #7

---
+++ src/main/java/aiss/vimeoMiner/service/VideoService.java
@@ -0,0 +1,84 @@
+package aiss.vimeoMiner.service;
+
+import aiss.vimeoMiner.vimeoModel.modelVideos.Video;
+import aiss.vimeoMiner.vimeoModel.modelChannel.Channel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import aiss.vimeoMiner.vimeoModel.modelVideos.Videos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class VideoService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    // Get from Vimeo API
+    public List<Video
+            > getVideos(String videosUri){
+        // URI
+        String uri = "https://api.vimeo.com" + videosUri;
+
+        // Header for authentication
+        HttpHeaders header = new HttpHeaders(){
+            {
+                String auth = "Bearer ee507ffdb4da956d56252e8eb067fb58";
+                set("Authorization", auth);
+            }
+        };
+
+        // Send message
+        ResponseEntity<Videos> response = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<Videos>(header),Videos.class);
+        Videos videos = response.getBody();
+        List<Video> videosArray = new ArrayList<>();
+        if (videos != null){
+            videosArray = videos.getData().stream().toList();
+        }
+
+        String nextUrl = getNextPageUrl(response.getHeaders());
+        while (nextUrl != null) {
+            response = restTemplate.exchange(nextUrl, HttpMethod.GET, new HttpEntity<Videos>(header), Videos.class);
+            videos = response.getBody();
+            videosArray.addAll(videos.getData().stream().toList());
+            nextUrl = getNextPageUrl(response.getHeaders());
+     }
+
+        return videosArray;
+    }
+
+
+    // Get next page URL
+    public static String getNextPageUrl(HttpHeaders headers) {
+        String result = null;
+
+        // If there is no link header, return null
+        List<String> linkHeader = headers.get("Link");
+        if (linkHeader == null)
+            return null;
+
+        // If the header contains no links, return null
+        String links = linkHeader.get(0);
+        if (links == null || links.isEmpty())
+            return null;
+
+        // Return the next page URL or null if none.
+        for (String token : links.split(", ")) {
+            if (token.endsWith("rel=\"next\"")) {
+                // Found the next page. This should look something like
+                // <https://api.github.com/repos?page=3&per_page=100>; rel="next"
+                int idx = token.indexOf('>');
+                result = token.substring(1, idx);
+                break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/aiss/vimeoMiner/service/VideoServiceTest.java
@@ -0,0 +1,23 @@
+package aiss.vimeoMiner.service;
+
+import aiss.vimeoMiner.vimeoModel.modelVideos.Video;
+import aiss.vimeoMiner.vimeoModel.modelVideos.Videos;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class VideoServiceTest {
+
+    @Autowired
+    VideoService service;
+    @Test
+    void getVideos() {
+        List<Video> videos = service.getVideos("/users/9096387/videos");
+        videos.forEach(v -> System.out.println(v.getName()));
+    }
+}
