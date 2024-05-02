40f86ad3fca03efdbe38b6cd647448e29e2c28e2
grnln <guillermo.rn04@gmail.com>
2024-05-02 10:06:32 +0200

    Implemented ChannelController and ChannelService GET requests (#1).

--- YouTubeMiner/src/main/java/aiss/youTubeMiner/controller/ChannelController.java
+++ YouTubeMiner/src/main/java/aiss/youTubeMiner/controller/ChannelController.java
@@ -1,4 +1,23 @@
 package aiss.youTubeMiner.controller;
 
+import aiss.youTubeMiner.model.channel.Channel;
+import aiss.youTubeMiner.service.ChannelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("youtubeMiner/api/v1/channels")
 public class ChannelController {
+    @Autowired
+    ChannelService channelService;
+
+    @GetMapping("{channelsId}")
+    public List<Channel> get(@PathVariable String channelsId) {
+        return channelService.getChannels(channelsId);
+    }
 }
--- YouTubeMiner/src/main/java/aiss/youTubeMiner/service/ChannelService.java
+++ YouTubeMiner/src/main/java/aiss/youTubeMiner/service/ChannelService.java
@@ -1,16 +1,45 @@
 package aiss.youTubeMiner.service;
 
 import aiss.youTubeMiner.model.channel.Channel;
+import aiss.youTubeMiner.model.channel.ChannelSearch;
+import aiss.youTubeMiner.model.videoSnippet.VideoSnippet;
+import aiss.youTubeMiner.model.videoSnippet.VideoSnippetSearch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ChannelService {
     @Autowired
     public RestTemplate restTemplate;
 
-    public Channel getChannel(String channelId) {
-        return null;
+    final String key = "AIzaSyCgo33WDq8_uoH6tWH6COhTmemxQbimDHY";
+
+    public List<Channel> getChannels(String channelsId) {
+        List<Channel> out = new ArrayList<>();
+
+        String uri = "https://www.googleapis.com/youtube/v3/channels";
+        uri += ("?id=" + channelsId);
+        uri += ("&part=" + "snippet");
+        uri += ("&key=" + key);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<ChannelSearch> request = new HttpEntity<>(headers);
+
+        ResponseEntity<ChannelSearch> response = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                request,
+                ChannelSearch.class
+        );
+
+        if (response.getBody() != null) {
+            out.addAll(response.getBody().getItems());
+        }
+        return out;
     }
 }
