a6e7922c1bd740430d0d59360433911cef603020
grnln <guillermo.rn04@gmail.com>
2024-05-02 13:37:26 +0200

    Implemented the VideoController (#18)

---
+++ VideoMiner/src/main/java/aiss/videoMiner/controller/VideoController.java
@@ -0,0 +1,52 @@
+package aiss.videoMiner.controller;
+
+import aiss.videoMiner.exception.ChannelNotFoundException;
+import aiss.videoMiner.exception.VideoNotFoundException;
+import aiss.videoMiner.model.Channel;
+import aiss.videoMiner.model.Video;
+import aiss.videoMiner.repository.ChannelRepository;
+import aiss.videoMiner.repository.VideoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/videoMiner/v1")
+public class VideoController {
+    @Autowired
+    VideoRepository videoRepository;
+
+    @Autowired
+    ChannelRepository channelRepository;
+
+    @GetMapping
+    public List<Video> findAll() {
+        return videoRepository.findAll();
+    }
+
+    @GetMapping("/videos/{videoId}")
+    public Video findOne(@PathVariable String videoId) throws VideoNotFoundException {
+        return videoRepository.findById(videoId).orElseThrow(VideoNotFoundException::new);
+    }
+
+    @GetMapping("/channels/{channelId}/videos")
+    public List<Video> findByChannel(@PathVariable Long channelId) throws ChannelNotFoundException {
+        return channelRepository.findById(channelId).orElseThrow(ChannelNotFoundException::new).getVideos();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/channels/{channelId}/videos")
+    public Video create(@PathVariable Long channelId, @Valid @RequestBody Video video) throws ChannelNotFoundException {
+        Channel channel = channelRepository.findById(channelId).orElseThrow(ChannelNotFoundException::new);
+        List<Video> videos = channel.getVideos();
+
+        videos.add(video);
+        channel.setVideos(videos);
+
+        channelRepository.save(channel);
+        return videoRepository.save(video);
+    }
+}
--- VideoMiner/src/main/java/aiss/videoMiner/repository/VideoRepository.java
+++ VideoMiner/src/main/java/aiss/videoMiner/repository/VideoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VideoRepository extends JpaRepository<Video,Long> {
+public interface VideoRepository extends JpaRepository<Video, String> {
 }
