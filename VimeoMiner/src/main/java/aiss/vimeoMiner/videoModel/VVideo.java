cb3245311d8e114d7e255cad13aa1ba3f4392edc
maquirosq <mquirosquiroga@gmail.com>
2024-05-02 15:32:06 +0200

    Added VideoMiner models to VimeoMiner for transformation

--- VimeoMiner/src/main/java/aiss/vimeoMiner/videoModel/VVideo.java
+++ VimeoMiner/src/main/java/aiss/vimeoMiner/videoModel/VVideo.java
@@ -1,10 +1,12 @@
 package aiss.vimeoMiner.videoModel;
 
+import java.util.List;
+
 public class VVideo {
     private String id;
     private String name;
     private String description;
     private String releaseTime;
-    // private List<VComment> comments;
-    // private List<VCaption> captions;
+    private List<VComment> comments;
+    private List<VCaption> captions;
 }
