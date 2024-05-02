c62a0685965796bd3fc5d0db3c716d57e81d592d
grnln <guillermo.rn04@gmail.com>
2024-05-02 14:17:05 +0200

    Implemented CaptionService and CaptionController (#3)

--- YouTubeMiner/src/main/java/aiss/youTubeMiner/controller/CaptionController.java
+++ YouTubeMiner/src/main/java/aiss/youTubeMiner/controller/CaptionController.java
@@ -1,4 +1,23 @@
 package aiss.youTubeMiner.controller;
 
+import aiss.youTubeMiner.model.caption.Caption;
+import aiss.youTubeMiner.service.CaptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("youtubeMiner/api/v1/captions")
 public class CaptionController {
+    @Autowired
+    CaptionService captionService;
+
+    @GetMapping("{videoId}")
+    public List<Caption> get(@PathVariable String videoId) {
+        return captionService.getCaptions(videoId);
+    }
 }
--- YouTubeMiner/src/main/java/aiss/youTubeMiner/service/CaptionService.java
+++ YouTubeMiner/src/main/java/aiss/youTubeMiner/service/CaptionService.java
@@ -1,16 +1,42 @@
 package aiss.youTubeMiner.service;
 
 import aiss.youTubeMiner.model.caption.Caption;
+import aiss.youTubeMiner.model.caption.CaptionSearch;
+import aiss.youTubeMiner.model.videoSnippet.VideoSnippet;
+import aiss.youTubeMiner.model.videoSnippet.VideoSnippetSearch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CaptionService {
     @Autowired
     RestTemplate restTemplate;
 
-    public Caption getCaption(String captionId) {
-        return null;
+    final String key = "AIzaSyCgo33WDq8_uoH6tWH6COhTmemxQbimDHY";
+
+    public List<Caption> getCaptions(String videoId) {
+        String uri = "https://www.googleapis.com/youtube/v3/captions";
+        uri += ("?videoId=" + videoId);
+        uri += ("&part=" + "snippet");
+        uri += ("&key=" + key);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<CaptionSearch> request = new HttpEntity<>(headers);
+
+        ResponseEntity<CaptionSearch> response = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                request,
+                CaptionSearch.class
+        );
+        return response.getBody().getItems();
     }
 }
