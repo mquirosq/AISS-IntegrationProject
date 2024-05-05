87ae376ab625bf2b92f07d89609ee2938320ab20
Alejandro Parody <alejandroparodyq@gmail.com>
2024-05-05 16:27:09 +0200

    Added GET and POST for Comments

---
+++ VideoMiner/src/main/java/aiss/videoMiner/controller/CommentController.java
@@ -0,0 +1,53 @@
+package aiss.videoMiner.controller;
+
+import aiss.videoMiner.exception.CommentNotFoundException;
+import aiss.videoMiner.exception.VideoNotFoundException;
+import aiss.videoMiner.model.Comment;
+import aiss.videoMiner.model.Video;
+import aiss.videoMiner.repository.CommentRepository;
+import aiss.videoMiner.repository.VideoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/videoMiner/v1")
+public class CommentController {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Autowired
+    VideoRepository videoRepository;
+
+    @GetMapping("/comments")
+    public List<Comment> findAll() {
+        return commentRepository.findAll();
+    }
+
+    @GetMapping("/comments/{commentId}")
+    public Comment findOne(@PathVariable String commentId) throws CommentNotFoundException {
+        return commentRepository.findById(Long.valueOf(commentId)).orElseThrow(CommentNotFoundException::new);
+    }
+
+    @GetMapping("/channels/{channelId}/videos/{videoId}/comments")
+    public List<Comment> findByVideo(@PathVariable String videoId) throws VideoNotFoundException {
+        return videoRepository.findById(videoId).orElseThrow(VideoNotFoundException::new).getComments();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/channels/{channelId}/videos/{videoId}/comments")
+    public Comment create(@PathVariable String videoId, @Valid @RequestBody Comment comment) throws VideoNotFoundException {
+        Video video = videoRepository.findById(videoId).orElseThrow(VideoNotFoundException::new);
+        List<Comment> comments = video.getComments();
+
+        comments.add(comment);
+        video.setComments(comments);
+
+        videoRepository.save(video);
+        return commentRepository.save(comment);
+    }
+}
