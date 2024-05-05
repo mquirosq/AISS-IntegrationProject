1f9d74aa645acff5c819c60882b53cf49d84bfc1
maquirosq <mquirosquiroga@gmail.com>
2024-05-05 18:40:13 +0200

    Added comments and captions to populate method

--- VimeoMiner/src/main/java/aiss/vimeoMiner/controller/ChannelController.java
+++ VimeoMiner/src/main/java/aiss/vimeoMiner/controller/ChannelController.java
@@ -1,14 +1,16 @@
 package aiss.vimeoMiner.controller;
 
-import aiss.vimeoMiner.exception.ChannelNotFoundException;
-import aiss.vimeoMiner.exception.VideoMinerConnectionRefusedException;
-import aiss.vimeoMiner.exception.VideoNotFoundException;
+import aiss.vimeoMiner.exception.*;
 import aiss.vimeoMiner.service.CaptionService;
 import aiss.vimeoMiner.service.CommentService;
 import aiss.vimeoMiner.service.VideoService;
+import aiss.vimeoMiner.videoModel.VCaption;
 import aiss.vimeoMiner.videoModel.VChannel;
+import aiss.vimeoMiner.videoModel.VComment;
 import aiss.vimeoMiner.videoModel.VVideo;
+import aiss.vimeoMiner.vimeoModel.modelCaption.Caption;
 import aiss.vimeoMiner.vimeoModel.modelChannel.Channel;
+import aiss.vimeoMiner.vimeoModel.modelComment.Comment;
 import aiss.vimeoMiner.vimeoModel.modelVideos.Video;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -47,18 +49,29 @@ public class ChannelController {
     public VChannel populateOne(@PathVariable String channelId,
                                 @RequestParam(name = "maxVideos", defaultValue = "10") Integer maxVideos,
                                 @RequestParam(name = "maxComments", defaultValue = "10") Integer maxComments)
-            throws ChannelNotFoundException, VideoMinerConnectionRefusedException, VideoNotFoundException {
+            throws ChannelNotFoundException, VideoMinerConnectionRefusedException, VideoNotFoundException, CommentNotFoundException, CaptionNotFoundException {
 
         Channel channel = channelService.getChannel(String.valueOf(channelId));
         VChannel createdChannel = channelService.createChannel(channel);
         if (maxVideos > 0){
-            List<Video> videoList = videoService.getVideos(channel.getMetadata().getConnections().getVideos().getUri());
-            for (Video v : videoList.subList(0, Math.min(maxVideos, videoList.size()))) {
+            List<Video> videos = videoService.getVideos(channel.getMetadata().getConnections().getVideos().getUri());
+            for (Video v : videos.subList(0, Math.min(maxVideos, videos.size()))) {
                 if (v != null) {
                     VVideo vVideo = videoService.createVideo(v, channelId);
                     createdChannel.getVideos().add(vVideo);
+
+                    List<Caption> captions = captionService.getCaptions(v.getMetadata().getConnections().getTexttracks().getUri());
+                    for (Caption caption : captions){
+                        VCaption vCaption = captionService.createCaption(caption, vVideo.getId());
+                        vVideo.getCaptions().add(vCaption);
+                    }
+
+                    List<Comment> comments = commentService.getComments(v.getMetadata().getConnections().getComments().getUri());
+                    for (Comment comment : comments.subList(0, Math.min(maxComments, comments.size()))){
+                        VComment vComment = commentService.createComment(comment, vVideo.getId());
+                        vVideo.getComments().add(vComment);
+                    }
                 }
-                // TODO: Implement max Comments
             }
         }
         return createdChannel;
