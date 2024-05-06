3b7076f28675907f7baeaec6832fe7311de5a684
maquirosq <mquirosquiroga@gmail.com>
2024-05-06 14:17:51 +0200

    Added user constructor

--- VideoMiner/src/main/java/aiss/videoMiner/model/User.java
+++ VideoMiner/src/main/java/aiss/videoMiner/model/User.java
@@ -30,6 +30,12 @@ public class User {
     @JsonProperty("picture_link")
     private String picture_link;
 
+    public User(String name, String user_link, String picture_link) {
+        this.name = name;
+        this.user_link = user_link;
+        this.picture_link = picture_link;
+    }
+
     public Long getId() {
         return id;
     }
