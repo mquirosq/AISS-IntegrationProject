236b58a90e350cbb9c1a0e3edabecf0b0f18d8c5
alicjarl <alusia.rygiel@gmail.com>
2024-05-07 12:07:30 +0200

    POST user to VimeoMiner

--- VimeoMiner/src/main/java/aiss/vimeoMiner/service/UserService.java
+++ VimeoMiner/src/main/java/aiss/vimeoMiner/service/UserService.java
@@ -1,6 +1,9 @@
 package aiss.vimeoMiner.service;
 
 import aiss.vimeoMiner.exception.ChannelNotFoundException;
+import aiss.vimeoMiner.exception.VideoMinerConnectionRefusedException;
+import aiss.vimeoMiner.videoModel.VChannel;
+import aiss.vimeoMiner.videoModel.VUser;
 import aiss.vimeoMiner.vimeoModel.modelUser.ModelUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -8,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,8 +21,9 @@ public class UserService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public ModelUser getUser(String userId) throws ChannelNotFoundException {
-        String uri = "https://api.vimeo.com/users/" + userId;
+
+    public ModelUser getUser(String userUri) throws ChannelNotFoundException {
+        String uri = "https://api.vimeo.com/" + userUri;
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer ee507ffdb4da956d56252e8eb067fb58");
@@ -31,23 +36,32 @@ public class UserService {
             throw new ChannelNotFoundException();
         }
     }
-
-    public ModelUser postUser(ModelUser user) {
-        String uri = "https://api.vimeo.com/users/";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer ee507ffdb4da956d56252e8eb067fb58");
-
-        try {
-            HttpEntity<ModelUser> request = new HttpEntity<>(user, headers);
-            ResponseEntity<ModelUser> response = restTemplate.exchange(uri, HttpMethod.POST, request, ModelUser.class);
-            return response.getBody();
-        } catch (RestClientResponseException ex) {
-            // Handle exception appropriately
-            ex.printStackTrace();
-            return null;
-        }
+//post to VideoMiner
+    public VUser createUser(ModelUser modelUser) throws VideoMinerConnectionRefusedException {
+    String uri = "http://localhost:8080/videoMiner/v1/users";
+    try{
+        VUser vUser = transformUser(modelUser);
+        HttpEntity<VUser> request = new HttpEntity<>(vUser);
+        ResponseEntity<VUser> response = restTemplate.exchange(uri, HttpMethod.POST, request, VUser.class);
+        VUser createdUser = response.getBody();
+        return createdUser;
+    }
+    catch(RestClientResponseException err) {
+        System.out.println("Error when creating the user " + modelUser + ":"+ err.getLocalizedMessage());
+        return null;
+    }
+    catch(ResourceAccessException err){
+        throw new VideoMinerConnectionRefusedException();
     }
 }
+
+    private VUser transformUser(ModelUser modelUser) {
+        VUser vUser=new VUser();
+        vUser.setUser_link(modelUser.getLink());
+        vUser.setName(modelUser.getName());
+        vUser.setPicture_link(modelUser.getPictures().getBaseLink());
+        return vUser;
+    }
+    }
 
 
