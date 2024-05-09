f010f24e5fef004dd7c255b5e804f8b00137745f
maquirosq <mquirosquiroga@gmail.com>
2024-05-09 21:15:10 +0200

    Updated constants for vimeo and videoMiner

--- VimeoMiner/src/main/java/aiss/vimeoMiner/helper/ConstantsHelper.java
+++ VimeoMiner/src/main/java/aiss/vimeoMiner/helper/ConstantsHelper.java
@@ -3,6 +3,6 @@ package aiss.vimeoMiner.helper;
 public class ConstantsHelper {
     public static final String apiAuthorizationString = "Bearer ee507ffdb4da956d56252e8eb067fb58";
     public static final String apiBaseUri = "vimeoMiner/api/v1";
-    public static final String vimeoBaseUri = "https://www.googleapis.com/youtube/v3";
-    public static final String videoMinerBaseUri = "http://localhost:8080/videoMiner/v1";
+    public static final String vimeoBaseUri = "https://api.vimeo.com";
+    public static final String videoMinerBaseUri = "http://localhost:8080/videominer";
 }
